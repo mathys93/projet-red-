24f04df9bd584f225fde6389b3ddf9a192800f58
Mhenri <henri.mangin@ynov.com>
2026-09-21 10:01:09 +0200

    test

---
+++ initcharacter/main.go
@@ -0,0 +1,11 @@
+package initcharacter
+
+func InitCharacter(name string, AP int, DP int, INV string, LP int, XP int, MP int){
+	name = "personnage 1"
+	AP = 1
+	DP = 1
+	INV = "NONE"
+	LP = 20
+	XP = 0
+	MP = 0
+}
---
+++ main .go
@@ -0,0 +1,15 @@
+package main
+
+func InitCharacter(name string, AP int, DP int, INV string, LP int, XP int, MP int){
+	name = "personnage 1"
+	AP = 1
+	DP = 1
+	INV = "NONE"
+	LP = 20
+	XP = 0
+	MP = 0
+}
+
+func DisplayInfo(initCharacter interface{}) interface{} {
+	return initCharacter
+}
